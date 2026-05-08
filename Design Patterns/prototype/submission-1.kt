e26555dd80a0ea62848f3020fa29bea05e0369cd
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2026-05-08 16:54:29 -0300

    Add: prototype - submission-1

---
+++ Design Patterns/prototype/submission-1.kt
@@ -0,0 +1,26 @@
+interface Shape {
+    fun clone(): Shape
+}
+
+class Rectangle(private val width: Int, private val height: Int) : Shape {
+    fun getWidth(): Int = width
+    fun getHeight(): Int = height
+
+    override fun clone(): Shape {
+        return Rectangle(width, height)
+    }
+}
+
+class Square(private val length: Int) : Shape {
+    fun getLength(): Int = length
+
+    override fun clone(): Shape {
+        return Square(length)
+    }
+}
+
+class Test {
+    fun cloneShapes(shapes: List<Shape>): List<Shape> {
+        return shapes.map { it.clone() }
+    }
+}
